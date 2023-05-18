7d01546ea169e6aad4ae6c5f4d23d68623b286f6
Danila Kornev <danilakornev92@gmail.com>
2023-05-18 21:40:19 +0300

    Initial Commit

---
+++ CoreDateProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDateProject
+//
+//  Created by Danila Kornev on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
